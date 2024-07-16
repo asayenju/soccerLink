32fd7b2c84e8e545cc4c2cef2fdf45b618ff4139
asayenju <ashwinsayenju@gmail.com>
2024-07-16 22:58:00 +0700

    Per 90 min stats pushed to the left

--- frontend/src/components/AppearancesContainer.js
+++ frontend/src/components/AppearancesContainer.js
@@ -53,70 +53,68 @@ export default function AppearancesContainer({
   sub_off_per_90,
   sub_on_per_90,
 }) {
-  const renderTextField = (label, name, value, handleChange) => {
-    const handleInputChange = (e) => {
-      const { name, value } = e.target;
-      if (/^\d*$/.test(value)) {
-        handleChange(e);
-      }
-    };
-
-    return (
-      <Grid container spacing={2} alignItems="center">
-        <Grid item xs={3}>
-          <Typography variant="body1" sx={{ color: 'white' }}>
-            {label}
-          </Typography>
-        </Grid>
-        <Grid item xs={9}>
-          <WhiteBorderTextField
-            type="text"
-            name={name}
-            value={value}
-            onChange={handleInputChange}
-            InputLabelProps={{
-              shrink: true,
-            }}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  {isForm && (
-                    <>
-                      <WhiteIconButton
-                        aria-label="increase"
-                        size="small"
-                        onClick={() =>
-                          handleChange({
-                            target: { name, value: parseInt(value || '0') + 1 },
-                          })
-                        }
-                      >
-                        <ArrowUpwardIcon />
-                      </WhiteIconButton>
-                      <WhiteIconButton
-                        aria-label="decrease"
-                        size="small"
-                        onClick={() =>
-                          handleChange({
-                            target: {
-                              name,
-                              value: Math.max(parseInt(value || '0') - 1, 0),
-                            },
-                          })
-                        }
-                      >
-                        <ArrowDownwardIcon />
-                      </WhiteIconButton>
-                    </>
-                  )}
-                </InputAdornment>
-              ),
-            }}
-          />
-        </Grid>
-      </Grid>
-    );
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    if (/^\d*$/.test(value)) {
+      handleChange(e);
+    }
   };
+
+  const renderTextField = (label, name, value, handleChange) => (
+    <Grid container spacing={2} alignItems="center">
+      <Grid item xs={3}>
+        <Typography variant="body1" sx={{ color: 'white' }}>
+          {label}
+        </Typography>
+      </Grid>
+      <Grid item xs={9}>
+        <WhiteBorderTextField
+          type="text"
+          name={name}
+          value={value}
+          onChange={handleInputChange}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                {isForm && (
+                  <>
+                    <WhiteIconButton
+                      aria-label="increase"
+                      size="small"
+                      onClick={() =>
+                        handleChange({
+                          target: { name, value: parseInt(value || '0') + 1 },
+                        })
+                      }
+                    >
+                      <ArrowUpwardIcon />
+                    </WhiteIconButton>
+                    <WhiteIconButton
+                      aria-label="decrease"
+                      size="small"
+                      onClick={() =>
+                        handleChange({
+                          target: {
+                            name,
+                            value: Math.max(parseInt(value || '0') - 1, 0),
+                          },
+                        })
+                      }
+                    >
+                      <ArrowDownwardIcon />
+                    </WhiteIconButton>
+                  </>
+                )}
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Grid>
+    </Grid>
+  );
 
   const totalStats = (
     <Grid container spacing={2} direction="column">
@@ -171,28 +169,28 @@ export default function AppearancesContainer({
   const per90Stats = (
     <Grid container spacing={2} direction="column">
       <Grid item>
-        <Typography variant="body1">
-          Games per 90: {games_per_90 ? games_per_90.toFixed(2) : 'N/A'}
+        <Typography variant="body1" sx={{ textAlign: 'left', color: 'white' }}>
+          Games: {games_per_90 ? games_per_90.toFixed(2) : 'N/A'}
         </Typography>
       </Grid>
       <Grid item>
-        <Typography variant="body1">
-          Minutes per 90: {minutes_per_90 ? minutes_per_90.toFixed(2) : 'N/A'}
+        <Typography variant="body1" sx={{ textAlign: 'left', color: 'white' }}>
+          Minutes: {minutes_per_90 ? minutes_per_90.toFixed(2) : 'N/A'}
         </Typography>
       </Grid>
       <Grid item>
-        <Typography variant="body1">
-          Starts per 90: {starts_per_90 ? starts_per_90.toFixed(2) : 'N/A'}
+        <Typography variant="body1" sx={{ textAlign: 'left', color: 'white' }}>
+          Starts: {starts_per_90 ? starts_per_90.toFixed(2) : 'N/A'}
         </Typography>
       </Grid>
       <Grid item>
-        <Typography variant="body1">
-          Subbed Off per 90: {sub_off_per_90 ? sub_off_per_90.toFixed(2) : 'N/A'}
+        <Typography variant="body1" sx={{ textAlign: 'left', color: 'white' }}>
+          Subbed Off: {sub_off_per_90 ? sub_off_per_90.toFixed(2) : 'N/A'}
         </Typography>
       </Grid>
       <Grid item>
-        <Typography variant="body1">
-          Subbed On per 90: {sub_on_per_90 ? sub_on_per_90.toFixed(2) : 'N/A'}
+        <Typography variant="body1" sx={{ textAlign: 'left', color: 'white' }}>
+          Subbed On: {sub_on_per_90 ? sub_on_per_90.toFixed(2) : 'N/A'}
         </Typography>
       </Grid>
     </Grid>
