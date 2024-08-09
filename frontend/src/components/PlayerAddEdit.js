8102fabd2910f026359048f794a9718b67c77829
asayenju <ashwinsayenju@gmail.com>
2024-08-09 17:36:52 +0545

    Added save button to playeraddedit

--- frontend/src/components/PlayerAddEdit.js
+++ frontend/src/components/PlayerAddEdit.js
@@ -2,15 +2,37 @@ import React from 'react';
 import ResponsiveAppBar from './Header';
 import Footer from './footer';
 import StatsTabs from './StatsTab';
+import Button from '@mui/material/Button';
 
 const PlayerAddEdit = () => {
   return (
     <>
-        <ResponsiveAppBar />
-        <StatsTabs isForm={true}/>
-        <Footer />
+      <ResponsiveAppBar />
+      <StatsTabs isForm={true} />
+      <div 
+        style={{
+          display: 'flex', 
+          justifyContent: 'center', 
+          margin: '1vh'
+          // No need for alignItems as we want vertical position to remain natural
+        }}
+      >
+       <Button 
+        variant="contained" 
+        size="large" 
+        sx={{ 
+          width: '10%', 
+          backgroundColor: 'green',
+          '&:hover': {
+            backgroundColor: '#50C878', // Change hover color to light green
+          },
+        }}
+      >
+        SAVE
+      </Button>
+      </div>
+      <Footer />
     </>
-
   );
 };
 
